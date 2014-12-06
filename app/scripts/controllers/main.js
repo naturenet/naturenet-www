d3d30bb6caa6e701b311ecdb885d7d1a4f62b12c
Tom Yeh <doubleshow@gmail.com>
2014-12-06 07:11:24 -0700

    add lightbox to observations page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,6 +15,55 @@ angular.module('naturenetWebApp')
             'Karma'
         ];
 
+
+        var slides = $scope.slides = [];
+        $scope.addSlide = function() {
+            // var newWidth = 600 + slides.length + 1;
+            slides.push({
+                image: 'images/slide-' + i + '.jpg',
+                text: 'ACES'
+            });
+        };
+        for (var i = 1; i < 4; i++) {
+            $scope.addSlide(i);
+        }
+
+
+        // $http.get('data.json').success(function(data) {
+
+        //     var notes = data.data;
+
+        //     $scope.images = []
+        //     $scope.notes = notes.reverse().slice(0, 200).filter(function(x) {
+        //         return x.kind == 'FieldNote' && x.medias && x.medias.length > 0
+        //     });
+
+        //     $scope.notes.forEach(function(x, i) {
+        //         x.index = i
+        //     });
+
+        //     $scope.images = $scope.notes.map(function(x) {
+        //         return {
+        //             'url': x.medias[0].link,
+        //             'caption': 'some caption'
+        //         };
+        //     });
+
+        //     $scope.openLightboxModal = function(index) {
+        //         console.log('open')
+        //         Lightbox.openModal($scope.images, index);
+        //     };
+
+        // });
+
+
+        // $scope.currentPage = 1
+        // $scope.pageSize = 12
+
+
+    })
+    .controller('ObservationListCtrl', function($scope, $http, Lightbox) {
+
         $http.get('data.json').success(function(data) {
 
             var notes = data.data;
@@ -22,6 +71,22 @@ angular.module('naturenetWebApp')
             $scope.notes = notes.filter(function(x) {
                 return x.kind == 'FieldNote' && x.medias && x.medias.length > 0
             }).reverse().slice(0, 200);
+
+            $scope.notes.forEach(function(x, i) {
+                x.index = i
+            });
+
+            $scope.images = $scope.notes.map(function(x) {
+                return {
+                    'url': x.medias[0].link,
+                    'caption': x.content
+                };
+            });
+
+            $scope.openLightboxModal = function(index) {
+                console.log('open')
+                Lightbox.openModal($scope.images, index);
+            };
 
 
         });
@@ -30,31 +95,14 @@ angular.module('naturenetWebApp')
         $scope.pageSize = 12
 
     })
-	.controller('ObservationListCtrl', function($scope, $http) {
-
-        $http.get('data.json').success(function(data) {
-
-            var notes = data.data;
-
-            $scope.notes = notes.filter(function(x) {
-                return x.kind == 'FieldNote' && x.medias && x.medias.length > 0
-            }).reverse().slice(0, 200);
+    .controller('ObservationCtrl', function($scope, $http, $routeParams) {
 
 
+        var url = 'http://naturenet.herokuapp.com/api/note/' + $routeParams.id
+
+        $http.get(url).success(function(data) {
+            $scope.observation = data.data;
         });
-
-        $scope.currentPage = 1
-        $scope.pageSize = 12
-
-    })   
-    .controller('ObservationCtrl', function($scope, $http, $routeParams){
-
-
-    	var url = 'http://naturenet.herokuapp.com/api/note/' + $routeParams.id
-		
-		$http.get(url).success(function(data) {
-			$scope.observation = data.data;
-		});
 
     })
     .controller('OtherController', function($scope) {
